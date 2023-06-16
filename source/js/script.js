15dc6ea2c50646fbc99974771bfb851b5043119e
dmitriyvls <hlooba@mail.ru>
2023-06-16 18:58:28 +0300

    верстка 3 блоков

---
+++ source/js/script.js
@@ -0,0 +1,8 @@
+/* в этот файл добавляет скрипты*/
+const burger = document.querySelector('.burger__link-button');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', function(){
+  burger.classList.toggle ('burger__active');
+  menu.classList.toggle('menu__hidden');
+});
